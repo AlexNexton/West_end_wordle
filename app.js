bc9b18736a1a75e4d16f444b01ad58429080178f
AlexNexton <64314387+AlexNexton@users.noreply.github.com>
2022-03-04 00:32:26 +0000

    added tiles to the tile-container

--- app.js
+++ app.js
@@ -34,6 +34,30 @@ const keys = [
     '«',
 ]
 
+ 
+
+const guessRows = [
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', '']
+]
+
+guessRows.forEach((guessRow, guessRowIndex) => {
+    const rowElement = document.createElement('div');
+    rowElement.setAttribute('id', 'guessRow-' + guessRowIndex);
+    guessRow.forEach((guess, guessIndex) => {
+        const tileElement = document.createElement('div');
+        tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex);
+        tileElement.classList.add('tile');
+        rowElement.append(tileElement);
+
+    })
+    tileDisplay.append(rowElement);
+});
+
 const handleClick = () => {
     console.log('clicked')
 }
